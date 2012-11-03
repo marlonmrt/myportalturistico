479b73ca89250b9b36f29aaec3d3a527988a6154
julio.vilca@gmail.com <julio.vilca@gmail.com@f58ef82b-42ca-21a3-fec8-c1a42506340f>
2012-11-03 06:46:36 +0000

    Carga de la estructura inicial de la solución

---
+++ trunk/DSSOS/PaqueteTuristicoWeb/Models/TipoPaquete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaqueteTuristicoWeb.Models
+{
+    public class TipoPaquete
+    {
+        public int Codigo { get; set; }
+        public String Nombre { get; set; }
+    }
+}
