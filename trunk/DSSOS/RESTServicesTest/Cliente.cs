ddea1f83ae352fc3904e374e909f6ef6e97682ff
hackmage.x27@gmail.com <hackmage.x27@gmail.com@f58ef82b-42ca-21a3-fec8-c1a42506340f>
2012-11-28 00:47:44 +0000

    test de GET y POST (obtener OK y crear OK)

---
+++ trunk/DSSOS/RESTServicesTest/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RESTServicesTest
+{
+    public class Cliente
+    {
+        public string apellidoCli { get; set; }
+        public string correo { get; set; }
+        public string dni { get; set; }
+        public string nombreCli { get; set; }
+    }
+}
